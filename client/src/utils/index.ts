9418cdc764cc5e15fc1b2606ad177c1729184399
Moazam05 <salmanmoazam08@gmail.com>
2023-11-15 22:57:08 +0500

    formik form in signup screen

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,41 +1,6 @@
-export function parseAndFormatTimestamp(timestampString: string | Date | any) {
-  const timestamp = parseInt(timestampString, 10);
-
-  if (isNaN(timestamp)) {
-    console.error("Invalid timestamp string");
-    return null;
-  }
-
-  const dateObject = new Date(timestamp);
-
-  const options: Intl.DateTimeFormatOptions = {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    // timeZoneName: "short",
-  };
-
-  const formattedDateString = dateObject.toLocaleString("en-US", options);
-  return formattedDateString;
-}
-
 // prevent auto form submission
 export function onKeyDown(keyEvent: any) {
   if ((keyEvent.charCode || keyEvent.keyCode) === 13) {
     keyEvent.preventDefault();
   }
-}
-
-export function getInitials(name: string) {
-  // Split the name into parts
-  const nameParts = name.split(" ");
-
-  // Extract the first character of each part
-  const initials = nameParts.map((part) => part.charAt(0).toUpperCase());
-
-  // Join the initials together
-  return initials.join("");
 }
--- client/src/views/Signup/index.tsx
+++ client/src/views/Signup/index.tsx
@@ -1,10 +1,56 @@
-import React from "react";
-import { Box } from "@mui/material";
-import { Heading, SubHeading } from "../../components/Heading";
+// React Imports
+import { useState } from "react";
 import { Link } from "react-router-dom";
+// React Icons
+import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
+// Formik Imports
+import { Form, Formik, FormikProps } from "formik";
+// Utils Imports
+import { onKeyDown } from "../../utils";
+// Validation Schema
+import { signupSchema } from "./components/validationSchema";
+// MUI Imports
+import { Box, Button } from "@mui/material";
+// Custom Imports
+import { Heading, SubHeading } from "../../components/Heading";
+import ToastAlert from "../../components/ToastAlert/ToastAlert";
+import PrimaryInput from "../../components/PrimaryInput/PrimaryInput";
+// Images Imports
 import NextWhiteLogo from "../../assets/images/nexCenterLogo.svg";
 
+interface ISSignupForm {
+  name: string;
+  email: string;
+  password: string;
+}
+
 const Signup = () => {
+  // states
+  const [showPassword, setShowPassword] = useState(false);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [formValues, setFormValues] = useState<ISSignupForm>({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [toast, setToast] = useState({
+    message: "",
+    appearence: false,
+    type: "",
+  });
+
+  const hideShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleCloseToast = () => {
+    setToast({ ...toast, appearence: false });
+  };
+
+  const signupHandler = async (data: ISSignupForm) => {
+    console.log("payload", data);
+  };
+
   return (
     <>
       <Box
@@ -24,7 +70,6 @@ const Signup = () => {
             },
           }}
         >
-          {/* <Image src={NextWhiteLogo} alt="logo" layout="fixed" height={200} /> */}
           <img src={NextWhiteLogo} alt="logo" style={{ height: 200 }} />
         </Box>
 
@@ -141,12 +186,135 @@ const Signup = () => {
                 >
                   Create an Account
                 </Heading>
-                formik form
+                <Box>
+                  <Formik
+                    initialValues={formValues}
+                    onSubmit={(values: ISSignupForm) => {
+                      signupHandler(values);
+                    }}
+                    validationSchema={signupSchema}
+                  >
+                    {(props: FormikProps<ISSignupForm>) => {
+                      const {
+                        values,
+                        touched,
+                        errors,
+                        handleBlur,
+                        handleChange,
+                      } = props;
+
+                      return (
+                        <Form onKeyDown={onKeyDown}>
+                          <Box sx={{ marginBottom: "10px", marginTop: "20px" }}>
+                            <SubHeading sx={{ marginBottom: "5px" }}>
+                              Name
+                            </SubHeading>
+                            <PrimaryInput
+                              type="text"
+                              label=""
+                              name="name"
+                              placeholder="Name"
+                              value={values.name}
+                              helperText={
+                                errors.name && touched.name ? errors.name : ""
+                              }
+                              error={errors.name && touched.name ? true : false}
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                            />
+                          </Box>
+                          <Box sx={{ marginBottom: "10px" }}>
+                            <SubHeading sx={{ marginBottom: "5px" }}>
+                              Email
+                            </SubHeading>
+                            <PrimaryInput
+                              type="text"
+                              label=""
+                              name="email"
+                              placeholder="Email"
+                              value={values.email}
+                              helperText={
+                                errors.email && touched.email
+                                  ? errors.email
+                                  : ""
+                              }
+                              error={
+                                errors.email && touched.email ? true : false
+                              }
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                            />
+                          </Box>
+                          <Box sx={{ marginBottom: "10px" }}>
+                            <SubHeading sx={{ marginBottom: "5px" }}>
+                              Password
+                            </SubHeading>
+                            <PrimaryInput
+                              type={showPassword ? "text" : "password"}
+                              label=""
+                              name="password"
+                              placeholder="Password"
+                              value={values.password}
+                              helperText={
+                                errors.password && touched.password
+                                  ? errors.password
+                                  : ""
+                              }
+                              error={
+                                errors.password && touched.password
+                                  ? true
+                                  : false
+                              }
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              onClick={hideShowPassword}
+                              endAdornment={
+                                showPassword ? (
+                                  <AiOutlineEye color="disabled" />
+                                ) : (
+                                  <AiOutlineEyeInvisible color="disabled" />
+                                )
+                              }
+                            />
+                          </Box>
+                          <Box
+                            sx={{
+                              display: "flex",
+                              justifyContent: "end",
+                              marginTop: "20px",
+                            }}
+                          >
+                            <Button
+                              type="submit"
+                              variant="contained"
+                              fullWidth
+                              // disabled={loading}
+                              sx={{
+                                padding: "5px 30px",
+                                textTransform: "capitalize",
+                                margin: "20px 0",
+                              }}
+                            >
+                              {/* {loading ? "Sign Up..." : "Sign Up"} */}
+                              Sign Up
+                            </Button>
+                          </Box>
+                        </Form>
+                      );
+                    }}
+                  </Formik>
+                </Box>
               </Box>
             </Box>
           </Box>
         </Box>
       </Box>
+      <ToastAlert
+        appearence={toast.appearence}
+        type={toast.type}
+        message={toast.message}
+        handleClose={handleCloseToast}
+      />
     </>
   );
 };
