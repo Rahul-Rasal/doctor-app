0645b1f605d4835a5df533212c452ba6e811c8a5
Moazam05 <salmanmoazam08@gmail.com>
2023-11-23 15:04:56 +0500

    heroku configuration

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const colors = require("colors");
 const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
 // Custom Imports
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -34,6 +35,14 @@ app.get("/", (req, res) => {
 // ROUTES
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/doctors", doctorRouter);
+
+// Heroku Configuration
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
